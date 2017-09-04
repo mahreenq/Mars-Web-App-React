a86215121a4ca1fd3c559d37ec4b66ed3c07b245
Mahreen Qureshi <mahreenq@hotmail.com>
2017-09-04 15:10:41 -0400

    Fixed post encounter error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
-import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 import { BrowserRouter ,Route,Link } from 'react-router-dom'
@@ -11,12 +11,9 @@ import { BrowserRouter ,Route,Link } from 'react-router-dom'
 
 
 class App extends Component{
-
   constructor(props) {
     super(props);
   }
-
-
   render(){
     return (
       <BrowserRouter>
@@ -30,24 +27,18 @@ class App extends Component{
       </BrowserRouter>
     );
   }
-
 }
 
 
 
 
-
-
 class Register extends Component {
-
   constructor(props) {
     super(props);
-    this.state =
-              {
-                      name :"",
+    this.state =  {   name :"",
                       age: "",
                       jobs:[]
-              };
+                };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -55,14 +46,30 @@ class Register extends Component {
     this.setState(  { [event.target.name]: event.target.value });
   }
   handleSubmit(event) {
-
+  event.preventDefault();
    axios.post("https://red-wdp-api.herokuapp.com/api/mars/colonists",
    { "colonist":{"name":this.state.name,
                   "age": this.state.age,
                   "job" : this.state.job}
   })
+  .then(function(response) {
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
+});
 }
 
+componentDidMount(){
+ axios.get("https://red-wdp-api.herokuapp.com/api/mars/jobs")
+   .then(res => {
+     const jobs = res.data.jobs;
+     this.setState({ jobs});
+ })
+   .catch((error) => {
+     console.log(error);
+ })
+}
 
   render() {
     return (
@@ -76,13 +83,19 @@ class Register extends Component {
         <div className="justify-content-center flex padding-top-med">
               <form className="register" onSubmit={this.handleSubmit}>
 
-            <div className="regform flex justify-content-space-between"> NAME <input type="text" name="name"  value = {this.state.name} onChange={this.handleChange}/> </div>
+            <div className="regform flex justify-content-space-between">
+            NAME
+            <input type="text" name="name"  value = {this.state.name} onChange={this.handleChange}/>
+            </div>
 
-            <div className="regform flex justify-content-space-between"> AGE <input type="number" name="age" value={this.state.age} onChange={this.handleChange}/> </div>
+            <div className="regform flex justify-content-space-between">
+            AGE
+            <input type="number" name="age" value={this.state.age} onChange={(event) => this.handleChange(event)}/> </div>
 
 
-            <div className="regform flex justify-content-space-between"> OCCUPATION
-            <select value={this.state.value} onChange={this.handleChange} name="job">
+            <div className="regform flex justify-content-space-between">
+            OCCUPATION
+            <select value={this.state.job}  name="job" onChange={(event) => this.handleChange(event)}>
              <option value="" > Choose Job</option>
               {this.state.jobs.map(job =>
                 <option value={job.id} >{job.name}  </option>
@@ -90,27 +103,15 @@ class Register extends Component {
             </select>
             </div>
 
-            <Link to = '/getencounters'><input className="submit" type="submit" value="Submit" /></Link>
+            <Link to = '/getencounters'>
+            <input className="submit" type="submit" value="Check In" />
+            </Link>
               </form>
         </div>
-
-
         </div>
     );
-
   }
 
-
-  componentDidMount(){
-   axios.get("https://red-wdp-api.herokuapp.com/api/mars/jobs")
-     .then(res => {
-       const jobs = res.data.jobs;
-       this.setState({ jobs});
-   })
-     .catch((error) => {
-       console.log(error);
-   });
- }
 
 
 }
@@ -127,6 +128,26 @@ class ReportAliens extends Component{
           this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+
+    axios.post("https://red-wdp-api.herokuapp.com/api/mars/encounters",
+    { "encounter":{"atype":this.state.alien,
+                   "date": new Date(),
+                   "action" : this.state.action,
+                   "colonist_id": 2840}
+   })
+   .then(function(response) {
+   console.log(response);
+ })
+ .catch(function (error) {
+   console.log(error);
+ });
+  }
+
   componentDidMount(){
    axios.get("https://red-wdp-api.herokuapp.com/api/mars/aliens")
      .then(res => {
@@ -138,29 +159,17 @@ class ReportAliens extends Component{
    });
   }
 
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
 
-  handleSubmit(event) {
-
-    axios.post("https://red-wdp-api.herokuapp.com/api/mars/encounters",
-    { "encounter":{"atype":this.state.alien,
-                   "date": new Date(),
-                   "action" : this.state.action,
-                   "colonist_id": 5000}
-   })
-  }
   render() {
     return (
       <div className="reportAliensPage background flex justify-content-center">
-      
+
 
       <form onSubmit={this.handleSubmit}>
       <h1> Submit your alien encounter below </h1>
 
       <div className=" flex justify-content-space-between selectAlien"> WHICH ALIEN DID YOU SEE
-      <select value ={this.state.value} onChange={this.handleChange} name="alien">
+      <select name="alien" value ={this.state.value} onChange={this.handleChange} >
           <option value="" > Choose Alien</option>
         {this.state.aliens.map(alien =>
           <option value={alien.id} > {alien.type} </option>
@@ -172,7 +181,9 @@ class ReportAliens extends Component{
         <textarea className="flex" type="text" name="action"  value = {this.state.action} onChange={this.handleChange}> </textarea>
         </div>
 
-        <Link to = '/getencounters'><input className="submitReportAlien" type="submit" value="SUBMIT" /></Link>
+        <Link to = '/getencounters'>
+        <input className="submitReportAlien" type="submit" value="SUBMIT" onClick={this.handleSubmit} />
+        </Link>
       </form>
 
       <div className="navHome"> <Link to = '/'> HOME </Link></div>
@@ -214,9 +225,6 @@ class GetEncounters extends Component {
      <div><Link to = '/reportaliens'> HERE </Link></div>
      </div>
 
-
-
-
      <div className="getEncountersBody">
        {this.state.encounters.map(encounter =>
          <p key={encounter.id} >{encounter.date} {encounter.atype}
@@ -229,18 +237,6 @@ class GetEncounters extends Component {
    );
  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
